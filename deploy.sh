8021b5b6880c6f1148478484f1807539e8d328bd
Jozef Mlich <jmlich83@gmail.com>
2021-12-15 20:57:32 +0000

    update repos

--- deploy.sh
+++ deploy.sh
@@ -42,13 +42,15 @@ eval "$BUILD glacier-gallery-git"
 eval "$BUILD qt5-dbus-extended-git"
 eval "$BUILD qt5-mpris-git"
 eval "$BUILD glacier-music-git"
-
+eval "$BUILD glacier-packagemanager-git"
+eval "$BUILD glacier-polkit-agent"
 
 # homescreen/de
 eval "$BUILD mce-headers-git"
 eval "$BUILD libiphb-git"
 eval "$BUILD nemo-keepalive-git"
 eval "$BUILD nemo-qml-plugin-devicelock-git"
+eval "$BUILD glacier-devicelock-plugin"
 eval "$BUILD mlite"
 eval "$BUILD nemo-qml-plugin-configuration-git"
 eval "$BUILD nemo-qml-plugin-statusnotifier-git"
@@ -74,10 +76,10 @@ eval "$BUILD glacier-pinquery-git" # additional app
 eval "$BUILD qt5-ofono-nemo-extensions-git"
 eval "$BUILD qt5-connman-git"
 eval "$BUILD qt5-ngfd-git"
+eval "$BUILD ngfd-settings-nemo"
 eval "$BUILD qt5-usb-moded-git"
 eval "$BUILD profiled-settings-nemo-git"
 eval "$BUILD profiled-git"
-eval "$BUILD libsailfishkeyprovider-git"
 eval "$BUILD nemo-qml-plugin-systemsettings-git"
 eval "$BUILD qt5-lipstick-git"
 eval "$BUILD nemo-qml-plugin-connectivity-git"
@@ -93,7 +95,6 @@ eval "$BUILD qt-mobility-haptics-ffmemless-git"
 #multimedia stuff
 eval "$BUILD pulsecore-headers"
 eval "$BUILD pulseaudio-module-keepalive"
-eval "$BUILD pulseaudio-modules-nemo"
 
 eval "$BUILD libdsme"
 eval "$BUILD dsme-git"
@@ -111,11 +112,11 @@ eval "$BUILD fingerterm-git"
 
 eval "$BUILD glacier-testtool-git"
 eval "$BUILD ngfd-git"
+eval "$BUILD ngfd-plugin-native-vibrator"
 
 
 
 eval "$BUILD lipstick-glacier-home-git"
-eval "$BUILD pulseaudio-policy-enforcement"
 eval "$BUILD glacier-wayland-session"
 
 eval "$BUILD libcommhistory-git"
@@ -139,11 +140,33 @@ eval "$BUILD glacier-browser-git"
 # need to be clarified
 eval "$BUILD usb-tethering"
 
+
+# pure-maps packages
+if [ "$BUILDARCH" != "aarch64" ]; then
+    eval "$BUILD python-otherside"
+fi
+eval "$BUILD abseil-cpp-git"
+eval "$BUILD gpxpy-git"
+eval "$BUILD s2geometry-git"
+eval "$BUILD mapbox-gl-native-git"
+eval "$BUILD mapbox-gl-qml-git"
+eval "$BUILD pure-maps-git"
+
+eval "$BUILD glacier-weather-git"
+
+
+eval "$BUILD qt5-feedback-haptics-native-vibrator"
+eval "$BUILD libsocialcache"
+eval "$BUILD buteo-sync-plugins-social"
+
+
 # arm specific packages
 if [ "$BUILDARCH" == "aarch64" ]; then
+    eval "$BUILD bootsplash-theme-nemo" # for x86_64 plymouth-theme-
     eval "$BUILD hybris/android-headers"
     eval "$BUILD hybris/libhybris"
     eval "$BUILD hybris/qt5-qpa-hwcomposer-plugin"
 
     eval "$BUILD devices/nemo-device-pinephone"
+    eval "$BUILD devices/nemo-device-pinetab"
 fi
